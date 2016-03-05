f40fd20b5df3aedd1c5900cc4fbd07890239b817
ssowemim <ssowemim@ualberta.ca>
2016-03-05 00:04:28 -0700

    finished 3

--- a.pl
+++ a.pl
@@ -1,40 +1,26 @@
-likes(mary,food).
-likes(mary,wine).
-likes(john,wine).
-likes(john,mary).
 
-male(james1).
-male(charles1).
-male(charles2).
-male(james2).
-male(george1).
-
-female(catherine).
-female(elizabeth).
-female(sophia).
-
-parent(charles1, james1).
-parent(elizabeth, james1).
-parent(charles2, charles1).
-parent(catherine, charles1).
-parent(james2, charles1).
-parent(sophia, elizabeth).
-parent(george1, sophia).
-
+%QUESTION ONE
 % Used to sort list.
-xreverse(Inputlist,Outputlist):-
-    reverse(Inputlist,[],Outputlist).    
-    reverse([],Outputlist,Outputlist).    
+xreverse(Inputlist,Outputlist):- reverse(Inputlist,[],Outputlist).    
+reverse([],Outputlist,Outputlist).    
 
-    reverse([Head|Tail],List1,List2):-
-        reverse(Tail,[Head|List1],List2).
+reverse([Head|Tail],List1,List2):- reverse(Tail,[Head|List1],List2).
 
+%QUESTION TWO
+% Used to remove duplicates from list; this one was complicated
+set([], []).
+set([H|T], [H|T1]) :- remv(H, T, T2),set(T2, T1).
 
-% Used to remove duplicates from list
-make_set([],[]).
-   make_set(X,Y) :- setof(Z,member(Z,X),Y).
+remv(_, [], []).
+remv(X, [X|T], T1) :- remv(X, T, T1).
+remv(X, [H|T], [H|T1]) :- X \= H,remv(X, T, T1).
+
+%QUESTION THREE
+%combine two seperate lists
+xappend([],L,L). 
+xappend([H|T],L2,[H|L3])  :-  xappend(T,L2,L3).
 
 % combine unique elements from list
-xunion([X|Y],Z,W) :- member(X,Z),  xunion(Y,Z,W).
-xunion([X|Y],Z,[X|W]) :- \+ member(X,Z), xunion(Y,Z,W).
-xunion([],Z,Z).
+xunion(L1,L2,L3) :- xappend(L1,L2,L), set(L,L3).
+
+%QUESTION FOUR
